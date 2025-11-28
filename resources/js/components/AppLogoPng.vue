1c1d72939d65e2c90fae3632b9ab742cea5f10dc
Alex Atuncar <146157347+MrAleexx@users.noreply.github.com>
2025-11-27 19:04:13 -0500

    fix: bug en logos de produccion

--- resources/js/components/AppLogoPng.vue
+++ resources/js/components/AppLogoPng.vue
@@ -11,10 +11,10 @@ withDefaults(defineProps<Props>(), {
 <template>
     <div class="flex items-center gap-2">
         <!-- Logo para modo claro (texto oscuro) -->
-        <img src="/images/logos/Logo-Sinfondo-Dark.png" alt="MIS Library" class="w-auto block dark:hidden"
+        <img src="/images/logos/logo-Sinfondo-Dark.png" alt="MIS Library" class="w-auto block dark:hidden"
             :class="heightClass" />
         <!-- Logo para modo oscuro (texto blanco) -->
-        <img src="/images/logos/logo-Sinfondo-white.png" alt="MIS Library" class="w-auto hidden dark:block"
+        <img src="/images/logos/Logo-Sinfondo-white.png" alt="MIS Library" class="w-auto hidden dark:block"
             :class="heightClass" />
     </div>
 </template>
